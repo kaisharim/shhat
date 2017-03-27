60ff9b9760abcf8d828fc7059804c2b3191d2554
PC\Kais <kaisharim@gmail.com>
2017-03-27 12:56:53 +0200

    login and signup screen

---
+++ Shhat/app/src/main/java/com/example/kais/shhat/Models/ModelUser.java
@@ -0,0 +1,8 @@
+package com.example.kais.shhat.Models;
+
+/**
+ * Created by Kais on 27/03/2017.
+ */
+
+public class ModelUser {
+}
---
+++ Shhat/app/src/main/java/com/example/kais/shhat/Utils/ShhatConstants.java
@@ -0,0 +1,14 @@
+package com.example.kais.shhat.Utils;
+
+/**
+ * Created by Kais on 27/03/2017.
+ */
+
+public class ShhatConstants {
+    public class API {
+        public static final String APP_ID = "X-Parse-Application-Id";
+        public static final String REST_API_KEY = "X-Parse-REST-API-Key";
+        public static final String BASE_URL = "IP";
+        public static final String SERVER_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    }
+}
